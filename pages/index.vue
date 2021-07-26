bd47db353290c4cd8d43dc0f6d9ef5b542dfa43f
Danial <ds.1373@ymail.com>
2021-07-26 18:35:13 +0430

    Do the Task

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <!--  <Tutorial/>-->
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <h1>form</h1>
+        <!--        User Data form-->
+        <b-form @submit.prevent="onSubmit">
+          <b-col>
+            name
+            <b-input v-model="form.name">name</b-input>
+          </b-col>
+          <b-col>
+            family
+            <b-input v-model="form.family">family</b-input>
+          </b-col>
+          <b-col>
+            address
+            <b-input v-model="form.address">address</b-input>
+          </b-col>
+          <b-button type="submit">submit</b-button>
+        </b-form>
+        <nuxt-link to="/userdata">user data</nuxt-link>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        family: '',
+        address: '',
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      localStorage.setItem('userData', JSON.stringify(this.form))
+    },
+  },
+}
+</script>
---
+++ pages/userdata/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <!--        User data -->
+        <h1>User Data</h1>
+        <ul>
+          <li>Name: {{ getForm.name }}</li>
+          <li>Family: {{ getForm.family }}</li>
+          <li>Address: {{ getForm.address }}</li>
+        </ul>
+        <nuxt-link to="/">Back to Form</nuxt-link>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  computed: {
+    getForm() {
+      return localStorage.getItem('userData')
+        ? JSON.parse(localStorage.getItem('userData'))
+        : {}
+    },
+  },
+}
+</script>
+
+<style scoped></style>
